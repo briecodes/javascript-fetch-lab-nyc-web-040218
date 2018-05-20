1ba040369bd8b8d764d9f9b2a4a5f84350fe2979
briecodes <hello@brie.work>
2018-05-20 17:15:53 -0400

    work started.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const userName = ''
+
 function getIssues() {
 }
 
@@ -8,11 +10,22 @@ function createIssue() {
 }
 
 function showResults(json) {
+  $('#results').append(json.template())
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+
+  fetch(`https://api.github.com/repos/${userName}/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showResults(repo);
+  })
 }
 
 function getToken() {
