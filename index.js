adf5b40c314caf4229a4cc8c78183343c131b759
briecodes <hello@brie.work>
2018-05-24 09:28:36 -0400

    minimal advancements made.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const userName = ''
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
 
 function getIssues() {
 }
@@ -16,16 +18,38 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  
+    fetch(`${baseApi}repos/${repo}/forks`, {
+      method: 'post',
+      headers: {
+        'Authorization': `token ${getToken()}`
+      }
+    }).then(resp => {
+      let repo = new Repo(resp);
+      showResults(repo);
+    })
 
-  fetch(`https://api.github.com/repos/${userName}/${repo}/forks`, {
-    method: 'post',
-    headers: {
-      'Authorization': `token ${getToken()}`
-    }
-  }).then(resp => {
-    let repo = new Repo(resp);
-    showResults(repo);
-  })
+  // fetch(`https://api.github.com/repos/${userName}/${repo}/forks`, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Authorization': `token ${getToken()}`
+  //   }
+  // })
+  // .then( res => res.json() )
+  // .then(resp => {
+  //     let repo = resp.url;
+  //     showResults(repo)
+  // .then( response => console.log(response) );
+
+  // fetch(`https://api.github.com/repos/${userName}/${repo}/forks`, {
+  //   method: 'post',
+  //   headers: {
+  //     'Authorization': `token ${getToken()}`
+  //   }
+  // }).then(resp => {
+  //   let repo = resp.url;
+  //   showResults(repo);
+  // });
 }
 
 function getToken() {
